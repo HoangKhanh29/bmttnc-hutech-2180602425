e7529fcaa0751a1190ab971254f81014935da6f8
HoangKhanh29 <khanhhoag0@gmail.com>
2024-04-08 17:01:03 +0700

    [add] lab02 railfence

--- lab02/api.py
+++ lab02/api.py
@@ -1,8 +1,10 @@
-from cipher.vigenere import VigenereCipher 
 from flask import Flask, request, jsonify
 from cipher.caesar import CaesarCipher
-app = Flask(__name__)
+from cipher.vigenere import VigenereCipher
+from cipher.railfence import RailFenceCipher # Thêm vào phần đầu của file api.py
 
+app = Flask(__name__)
+#------------------------------------------CAESAR-------------------------------------------------
 #CAESAR CIPHER ALGORITHM
 caesar_cipher = CaesarCipher()
 
@@ -21,6 +23,7 @@ def caesar_decrypt():
     key = int(data['key'])
     decrypted_text = caesar_cipher.decrypt_text(cipher_text, key)
     return jsonify({'decrypted_message': decrypted_text})
+#--------------------------------------VIGENERE-----------------------------------------------------
 #VIGENERE CIPHER ALGORITHM
 vigenere_cipher = VigenereCipher()
 
@@ -38,7 +41,34 @@ def vigenere_decrypt():
     key = data['key']
     decrypted_text = vigenere_cipher.vigenere_decrypt(cipher_text, key)
     return jsonify({'decrypted_text': decrypted_text})
+#-------------------------------RAILFENCE------------------------------------------------------------
+# Thêm đoạn sau vào trước hàm main
+#RAILFENCE CIPHER ALGORITHM
+railfence_cipher = RailFenceCipher()
 
+@app.route('/api/railfence/encrypt', methods=['POST'])
+def encrypt():
+    data = request.json
+    plain_text = data['plain_text']
+    key = int(data['key'])
+    encrypted_text = railfence_cipher.rail_fence_encrypt(plain_text, key)
+    return jsonify({'encrypted_text': encrypted_text})
+@app.route('/api/railfence/decrypt', methods=['POST'])
+def decrypt():
+    data = request.json
+    cipher_text = data['cipher_text']
+    key = int(data['key'])
+    decrypted_text = railfence_cipher.rail_fence_decrypt(cipher_text, key)
+    return jsonify({'decrypted_text': decrypted_text})
+
+
+
+
+
+
+
+
+#-------------------------------------------------------------------------------------------
 #main function
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
